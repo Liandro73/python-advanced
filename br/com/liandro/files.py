9286d6758e08d33a395c6239eb2d029dc8ec1aab
Dernival Liandro <liandro.sys@gmail.com>
2025-07-06 21:18:22 +0100

    Working with files

---
+++ br/com/liandro/files.py
@@ -0,0 +1,32 @@
+# Reading lines as a List
+
+# file = open("./example.txt") # parameter "r"
+
+# lines = file.readlines()
+# for line in lines:
+#     print(line)
+
+
+# Reading the entire file
+
+# entire_file = file.read()
+# print(entire_file)
+
+# file.close()
+
+
+
+# Overwriting a file using the parameter "w"
+# file = open("./example_two.txt", "w")
+#
+# file.write("This is my file number two\n")
+# file.write("This is my file number two\n")
+#
+# file.close()
+
+
+
+# Inserting new lines to a file using the parameter "a"
+file = open("./example_two.txt", "a")
+
+file.write("This is my file number two\n")
