773df7961a51c0790c84e30d97a111cd7c553ffd
Dernival Liandro <liandro.sys@gmail.com>
2025-08-10 20:54:55 +0100

    List Comprehension II

--- br/com/liandro/list_comprehension.py
+++ br/com/liandro/list_comprehension.py
@@ -1,6 +1,10 @@
 x = [1, 2, 3, 4, 5, 6]
 y = [i**2 for i in x]
+z = [i for i in y if i%2==1]
+w = [i for i in y if i%2==0]
 
 print("Working with list comprehension")
 print(x)
 print(y)
+print(z)
+print(w)
